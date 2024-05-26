b49d0fb57201806850ce8abff7c01e68c01ea041
Arce-Mdina <nets_05basins@icloud.com>
2024-05-26 10:37:47 +0100

    Made things more dynamic

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
 import './App.css'
+import Education from './components/Education'
 
 function App() {
   
   return (
     <>
       <div className="header">
-        <h1><span className="header-1">Hi!</span> I am William!</h1>
-        <br></br>
-        <h2>Welcome to my <span className="header-2">Portfolio</span></h2>
+        <div className="title">
+          <h1><span className="header-1">Hi!</span> I am William!</h1>
+          <br></br>
+          <h2>Welcome to my <span className="header-2">Portfolio</span></h2>
+        </div>
       </div>
+
+      <br></br>
+
+      <Education />
     </>
   )
 }
