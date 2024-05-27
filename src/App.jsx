4059ef0971b4e9786b02403f519c914235470975
Arce-Mdina <nets_05basins@icloud.com>
2024-05-27 20:33:09 +0100

    Made some changes to the heading layout

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,11 @@ function App() {
     <>
       <div className="header">
         <div className="title">
-          <h1><span className="header-1">Hi!</span> I am William!</h1>
+          <h1>Welcome to my <span className="header-2">Portfolio</span></h1>
           <br></br>
-          <h2>Welcome to my <span className="header-2">Portfolio</span></h2>
+          <h2><span className="header-1">Hi!</span> I am William, a <span style={{ color: "#0a7ea4ff" }}>React JS</span> developer based in <span className="header-3">Europe</span></h2>
+          
+          
         </div>
       </div>
 
