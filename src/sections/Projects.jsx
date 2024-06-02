7268953c1d404020d8bff6aae89758395c5f755f
Arce-Mdina <nets_05basins@icloud.com>
2024-06-02 11:47:57 +0100

    Added some dynamic code

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import { projectsInfo } from './data-projects'
 
 const Projects = () => {
+
+    const projects = projectsInfo.map(project => 
+
+        <div key={project.id}>
+            {project.information}
+        </div>
+    
+    )
+
   return (
-    <div>Projects</div>
+    <div>
+        <h1>My Projects</h1>
+        <div className="card-grid">
+            {projects}
+        </div>
+    </div>
   )
 }
 
---
+++ src/sections/data-projects.jsx
@@ -0,0 +1,5 @@
+export const projectsInfo = [{
+    id: 0,
+    heading: "Project 1",
+    information: "info"
+}]
