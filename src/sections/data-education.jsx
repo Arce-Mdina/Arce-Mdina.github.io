a06ccd39ff62b3e0fc23ff7c0af1aa49217ec48a
Arce-Mdina <nets_05basins@icloud.com>
2024-06-03 15:41:40 +0100

    Added house captain and student council

--- src/sections/data-education.jsx
+++ src/sections/data-education.jsx
@@ -5,7 +5,7 @@ export const dcisInfo = [{
     information: (
         <>
             <div>
-                I was House Captain of Ubin for 4 years.
+                I was House Captain of Ubin for 4 consecutive years. And below is a list of all the things I have done.
             </div>
         </>
     ),
@@ -13,7 +13,17 @@ export const dcisInfo = [{
     id: 1,
     heading: "Student Council",
     information: (
-        <></>
+        <>
+            <div>
+                I was a member of the Digital & Learning Committee of Student Council for 2 years.
+            </div>
+            <div>
+                <br></br>
+                <p>There, I created 2/4 of the Microsoft tutorial videos (which is shown at the start of the 2023-24 school year) which helped students across campus to transfer from Google services to Microsoft services.</p>
+                <br></br>
+                <p>In addition, I contributed to planning a Hackathon and trying creating more recogniton and voice across NAE.</p>
+            </div>
+        </>
     ),
 }]
 
